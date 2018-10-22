a2ef23086e7256483b40a2db1ed36b06fa52ed54
APlowman <adam.jp@live.co.uk>
2018-10-22 23:06:39 +0100

    Add package development status classifier

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ from setuptools import find_packages, setup
 
 setup(
     name='vecmaths',
-    version="0.1",
+    version="0.1.0",
     description="A collection of (mostly vectorised) maths functions in Python.",
     author='Adam J Plowman, Maria S Yankova',
     packages=find_packages(),
     install_requires=[
         'numpy',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta'
     ],
 )
